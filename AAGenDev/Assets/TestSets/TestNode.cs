c8d861b02019d2b3667476885e2b5333be04e161
Mehdi <mehdi.hasheminia@unity3d.com>
2025-07-31 09:34:51 -0700

    Updates tests node system

--- AAGenDev/Assets/TestSets/TestNode.cs
+++ AAGenDev/Assets/TestSets/TestNode.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Create TestNode", fileName = "TestNode")]
-public class TestNode : ScriptableObject
+namespace AAGen
 {
-    public List<TestNode> Neighbors = new List<TestNode>();
-    
-#if UNITY_EDITOR
-    [ContextMenu("Validate Node")]
-    void Validate()
+    [CreateAssetMenu(menuName = "Create TestNode", fileName = "TestNode")]
+    public class TestNode : ScriptableObject
     {
-        // Check for self-reference
-        if (Neighbors.Contains(this))
-            Debug.LogError($"{name} has a self-reference.");
-    }
+        public List<TestNode> Neighbors = new List<TestNode>();
+
+#if UNITY_EDITOR
+        [ContextMenu("Validate Node")]
+        void Validate()
+        {
+            // Check for self-reference
+            if (Neighbors.Contains(this))
+                Debug.LogError($"{name} has a self-reference.");
+        }
 #endif
 
-    public void ConnectTo(TestNode node)
-    {
-        if (node == null)
+        public void ConnectTo(TestNode node)
         {
-            Debug.LogError($"node is null!");
-            return;
+            if (node == null)
+            {
+                Debug.LogError($"node is null!");
+                return;
+            }
+
+            if (Neighbors.Contains(node))
+            {
+                Debug.LogError($"Redundant edge: {name} is already connected to {node.name}");
+                return;
+            }
+
+            Neighbors.Add(node);
         }
-        
-        if (Neighbors.Contains(node))
-        {
-            Debug.LogError($"Redundant edge: {name} is already connected to {node.name}");
-            return;
-        }
-        
-        Neighbors.Add(node);
     }
 }
--- AAGenDev/Assets/TestSets/TestNodeGeneratorWindow.cs
+++ AAGenDev/Assets/TestSets/TestNodeGeneratorWindow.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
+using AAGen;
 
 public enum GraphTopology
 {
